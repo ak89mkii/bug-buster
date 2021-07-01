011dd0aa9bbec213b12d042cf9e515f275e517f4
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-07-01 11:43:22 -0400

    Virtual camera now tracks player. Added to level.

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    public int health = 1000;
+
+    public GameObject deathEffect;
+    
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if (health <= 0) {
+            Die();
+        }
+    }
+
+    // Update is called once per frame
+    void Die()
+    {
+        Instantiate(deatEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
